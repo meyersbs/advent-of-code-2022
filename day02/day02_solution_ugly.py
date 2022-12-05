a06a5865a29e28f824caed26fb04a303c7cae88d
Benjamin S. Meyers <meyers.bs@gmail.com>
2022-12-05 10:59:24 -0500

    Fix day02

--- day02/day02_solution_ugly.py
+++ day02/day02_solution_ugly.py
@@ -1,6 +1,8 @@
 import sys
-SCORE_MAP = { "AX": [4, 4], "AY": [1, 8], "AZ": [7, 3], "BX": [8, 1], "BY": [5, 5], "BZ": [2, 9], "CX": [3, 7], "CY": [9, 2], "CZ": [6, 6] }
-DETERMINE_HANDS = { "AX": "Z", "AY": "X", "AZ": "Y", "BX": "X", "BY": "Y", "BZ": "Z", "CX": "Y", "CY": "Z", "CZ": "X" }
+SCORE_MAP = { "AX": [4, 4], "AY": [1, 8], "AZ": [7, 3], "BX": [8, 1], "BY": [5, 5], "BZ": [2, 9],
+    "CX": [3, 7], "CY": [9, 2], "CZ": [6, 6] }
+DETERMINE_HANDS = { "AX": "Z", "AY": "X", "AZ": "Y", "BX": "X", "BY": "Y", "BZ": "Z", "CX": "Y",
+    "CY": "Z", "CZ": "X" }
 SCORES_PART1 = { "player1": [], "player2": [] }
 SCORES_PART2 = { "player1": [], "player2": [] }
 with open(sys.argv[1], "r") as f:
@@ -12,5 +14,7 @@ with open(sys.argv[1], "r") as f:
         scores = SCORE_MAP[hands[0] + DETERMINE_HANDS[hands[0] + hands[1]]]
         SCORES_PART2["player1"].append(scores[0])
         SCORES_PART2["player2"].append(scores[1])
-print("Player 1: {} | Player 2: {}".format(sum(SCORES_PART1["player1"]), sum(SCORES_PART1["player2"])))
-print("Player 1: {} | Player 2: {}".format(sum(SCORES_PART2["player1"]), sum(SCORES_PART2["player2"])))
+print("Player 1: {} | Player 2: {}".format(sum(SCORES_PART1["player1"]),
+    sum(SCORES_PART1["player2"])))
+print("Player 1: {} | Player 2: {}".format(sum(SCORES_PART2["player1"]),
+    sum(SCORES_PART2["player2"])))
